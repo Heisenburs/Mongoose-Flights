e0bbc865a9315c3d829d73f3f0beac6df88e1133
Heisenburs~ <jada.burs@gmail.com>
2023-04-13 13:54:20 -0400

    Flight.js (flightSchema) created & incorporated into server.js

---
+++ models/Flight.js
@@ -0,0 +1,20 @@
+//! Schema
+
+const mongoose = require('mongoose')
+//TODO: airline (enum to include american, sw, united), flight no (min 10- max 9999), depart date
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        enum: ['United', 'Southwest', 'American']
+    },
+    flightNo: {
+        type: Number,
+        minLength: 10,
+        maxLength: 9999
+    },
+    lastActiveAt: Date
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+module.exports = Flight;
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const connectToDb = require('./config/db')
 //TODO: Schema
+const Flight = require('./models/Flight')
 const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
     res.send('Flights Homepage')
+})
+
+app.get('/flightsindex', (req, res) => {
+    // res.send()
+    console.log('Flight Index')
 })
 
 app.listen(PORT, () => {
